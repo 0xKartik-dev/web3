168448f39db85b02016b092a734d6cc348678d8c
0xKartik-dev <karthikganiga007@gmail.com>
2022-08-26 02:26:04 +0530

    struct pure modifiers events

---
+++ datatypes/pureview.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract ViewAndPure {
+    // Declare a state variable
+    uint256 public x = 1;
+
+    // Promise not to modify the state (but can read state)
+    function addToX(uint256 y) public view returns (uint256) {
+        return x + y;
+    }
+
+    // Promise not to modify or read from state
+    function add(uint256 i, uint256 j) public pure returns (uint256) {
+        return i + j;
+    }
+}
