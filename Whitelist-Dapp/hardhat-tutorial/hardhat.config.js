5715d8e600e0f5a7ab23faf45c35614639065c7a
0xKartik-dev <karthikganiga007@gmail.com>
2022-09-04 16:21:48 +0530

    hardhat config

--- Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
+++ Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: "env" });
 
-const ALCHEMY_API_KEY_URL = "https://eth-rinkeby.alchemyapi.io/v2/replaced";
+const ALCHEMY_API_KEY_URL =
+  "https://eth-rinkeby.alchemyapi.io/v2/pRoXHc29FCR8mTtTWWUwPS4kFk04P67d";
 
-const RINKEBY_PRIVATE_KEY = "replaced";
+const RINKEBY_PRIVATE_KEY =
+  "f04f5d6a844e8a4fd7666fd1e1ac6e201812eee88da8992fa4fa34620e0ebde2";
 
 module.exports = {
   solidity: "0.8.9",
