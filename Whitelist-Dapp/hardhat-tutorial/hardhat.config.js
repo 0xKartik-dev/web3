ee820b48486aabcf529a00f6a113640cd0f714ec
0xKartik-dev <karthikganiga007@gmail.com>
2022-09-04 15:56:08 +0530

    added abi

--- Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
+++ Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
@@ -1,12 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: "env" });
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const ALCHEMY_API_KEY_URL = "https://eth-rinkeby.alchemyapi.io/v2/replaced";
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const RINKEBY_PRIVATE_KEY = "replaced";
 
 module.exports = {
-  solidity: "^0.8.1",
+  solidity: "0.8.9",
   networks: {
     rinkeby: {
       url: ALCHEMY_API_KEY_URL,
--- Whitelist-Dapp/my-app/constants/index.js
+++ Whitelist-Dapp/my-app/constants/index.js
@@ -1,2 +1,48 @@
-export const WHITELIST_CONTRACT_ADDRESS = "YOUR_WHITELIST_CONTRACT_ADDRESS";
-export const abi = YOUR_ABI;
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x3BC2352CDE1E30b673B0Ee41972C0876cB3a903c";
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "whitListAddressCounter",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
