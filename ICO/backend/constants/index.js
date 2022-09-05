e4f578ba1f3d2fd6461e345bfe36ba68e6121d1b
0xKartik-dev <karthikganiga007@gmail.com>
2022-09-06 00:34:34 +0530

    deploy contract

---
+++ ICO/backend/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x9801aE47C5232c25C186385CA21Fd0b4DdC159D3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
