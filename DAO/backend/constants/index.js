c3adbf6358e15cb295a1612dba7462cc9cb969bd
0xKartik-dev <karthikganiga007@gmail.com>
2022-09-06 23:43:38 +0530

    deploy script for dao

---
+++ DAO/backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9801aE47C5232c25C186385CA21Fd0b4DdC159D3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
