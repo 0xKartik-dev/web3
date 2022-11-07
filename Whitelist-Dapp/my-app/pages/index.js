9017d6e7bc7ec6ed4f2a3dab0d5c2deb89b7d540
0xKartik-dev <karthikganiga007@gmail.com>
2022-11-07 10:30:13 +0530

    changed contracct

--- Whitelist-Dapp/my-app/pages/index.js
+++ Whitelist-Dapp/my-app/pages/index.js
@@ -199,7 +199,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <h1 className={styles.title}>Welcome to EY WHitelist DApp!</h1>
           <div className={styles.description}>
             Its an NFT collection for developers in Crypto.
           </div>
@@ -213,9 +213,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
+      <footer className={styles.footer}>Made with &#10084; by EY</footer>
     </div>
   );
 }
